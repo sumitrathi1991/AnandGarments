278fe4315795f063cb56eccaa0ee89103279f303
arvind-das <arvind.dass@oodlestechnologies.com>
2014-06-22 12:45:30 +0530

    Item Domain created

---
+++ grails-app/domain/com/anand/Inventory/Item.groovy
@@ -0,0 +1,40 @@
+package com.anand.Inventory
+
+class Item {
+	
+	String itemName
+	String itemCode
+	String itemSize
+	String itemColor
+	String itemDescription
+	String itemBrand
+	String ItemType
+	float itemPrice
+	float discountRate
+	int quantity
+	int qtyOrdered
+	int qtyPending
+	boolean isItemDiscountable
+	
+	static constraints = {
+		itemSize nullable:true 
+		itemColor nullable:true 
+		itemDescription nullable:true 
+	}
+	
+	float getSalePrice(){
+		float salePrice = 0.00
+		if(isItemDiscountable)
+		salePrice = this.itemPrice - (this.itemPrice * (this.discountRate / 100))
+		else 
+		salePrice = this.itemPrice
+		return salePrice
+	}
+	
+	int getAvailableQuantity(){
+		return (this.quantity - (this.qtyOrdered + this.qtyPending))
+	}
+	
+	
+    
+}
